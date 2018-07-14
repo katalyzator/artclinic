ab61e1fdcf6c6d1bf3b3a58a4054fabbecd638fd
katalyzator <web.coder96@gmail.com>
2018-07-14 17:04:37 +0600

    added views

--- main/views.py
+++ main/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
+from main.models import Action, AboutImage, Specialist
+
 
 def index_view(request):
-    context = {}
+    actions = Action.objects.all()
+    slider_images = AboutImage.objects.all()
+    specialists = Specialist.objects.all()
+    context = {"actions": actions, "slider_images": slider_images, "specialists": specialists}
     template = 'index.html'
 
     return render(request, template, context)
